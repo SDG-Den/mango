59879c716dea28dba3e0a09be5c3a49ad552cb03
SDGDen <sdgden@sdgcloud.nl>
2026-09-03 20:52:47 +0200

    fixed re-arrange miss on early return path for groupdisband (affected single windows initialized with groupinit, resulting in them not resizing after the bar is removed)

--- src/dispatch/bind_define.h
+++ src/dispatch/bind_define.h
@@ -392,6 +392,8 @@ void groupdisband(const Arg *arg) {
 	if (!target->group_prev && !target->group_next) {
 		if (target->isgroupfocusing) {
 			target->isgroupfocusing = false;
+			focusclient(target, 1);
+			arrange(target->mon, false, false);
 		}
 		return;
 	}
